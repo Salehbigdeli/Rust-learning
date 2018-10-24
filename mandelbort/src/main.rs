e84dfd4fc5b2483897ae61711891430d463a8075
Aliakbar Saleh bigdeli <al.ak.saleh@gmail.com>
2018-10-24 19:54:25 +0330

    Complex struct

--- mandelbort/src/main.rs
+++ mandelbort/src/main.rs
@@ -1,3 +1,20 @@
+extern crate num;
+use num::complex;
+
+struct Complex<T>{
+    /// Real portion of complex number
+    re: T,
+    /// Imaginary portion of the complex number
+    im: T
+}
+
+#[allow(dead_code)]
+fn complex_square_add_loop(c: Complex<f64>){
+    let mut z = Complex{re: 0.0, im: 0.0};
+    loop {
+        z = z * z + c;
+    }
+}
 fn square_loop(mut x: f64){
     loop {
         x = x * x;
