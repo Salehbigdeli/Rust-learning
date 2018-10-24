138d35ea9eb66f9f85e40dcc26c1c6b834b6b315
Aliakbar Saleh bigdeli <al.ak.saleh@gmail.com>
2018-10-24 19:36:38 +0330

    Infinite loops

--- mandelbort/src/main.rs
+++ mandelbort/src/main.rs
@@ -1,3 +1,16 @@
+fn square_loop(mut x: f64){
+    loop {
+        x = x * x;
+    }
+}
+
+fn square_add_loop(c: f64){
+    let mut x: f64 = 0.;
+    loop {
+        x = x * x + c;
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
